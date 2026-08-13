b426e14a5b48fc118462ada00668a4a33116631e
Avinash Bakka <avinash.avinashb79@gmail.com>
2026-08-13 13:46:39 -0400

    Update wage calculator download links

---
+++ backend/modules/wagecalculator_web/exceptions.py
@@ -0,0 +1,24 @@
+"""
+Special mappings used by the Recruiter's Toolkit
+"""
+
+# --------------------------------------------------
+# Counties that need automatic correction
+# --------------------------------------------------
+
+COUNTY_FIXES = {
+
+    "Capitol County": "Hartford County",
+
+}
+
+# --------------------------------------------------
+# Special FLAG Area Names
+# --------------------------------------------------
+
+SPECIAL_AREAS = {
+
+    ("CONNECTICUT", "Hartford County"):
+        "CAPITOL PLANNING REGION",
+
+}
